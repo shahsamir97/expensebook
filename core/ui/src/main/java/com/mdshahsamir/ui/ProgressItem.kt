4abd0f3973de4016ac97228a6eb1060d1899dce1
Md. Shah Samir <shah.samir@therapservices.net>
2024-09-23 16:00:59 +0600

    Create custom progressbar with title

--- core/ui/src/main/java/com/mdshahsamir/ui/ProgressItem.kt
+++ core/ui/src/main/java/com/mdshahsamir/ui/ProgressItem.kt
@@ -1,15 +1,44 @@
 package com.mdshahsamir.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ProgressItem() {
-
+fun ProgressItem(title: String, progress: Float) {
+    ElevatedCard(
+        colors = CardDefaults.elevatedCardColors(),
+        onClick = { /*TODO*/ }
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                progress = { progress },
+                strokeWidth = 12.dp,
+                trackColor = MaterialTheme.colorScheme.inversePrimary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = title, style = MaterialTheme.typography.titleMedium)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun ProgressItemPreview() {
-    ProgressItem()
+    ProgressItem("Grocery", .7f)
 }
