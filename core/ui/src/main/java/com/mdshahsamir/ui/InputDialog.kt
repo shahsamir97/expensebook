00a6a8f753c7c0d85ec328030207c7705b4b08b1
Md. Shah Samir <mdshahsamir@gmail.com>
2024-11-06 02:27:36 +0600

    Handle EditCategoryDialog input error and add ime option

--- core/ui/src/main/java/com/mdshahsamir/ui/InputDialog.kt
+++ core/ui/src/main/java/com/mdshahsamir/ui/InputDialog.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -168,6 +169,7 @@ fun EditCategoryDialog(
     var titleInput by rememberSaveable { mutableStateOf(title) }
     var budgetInput by rememberSaveable { mutableStateOf(budget.toString()) }
     var spendAmountInput by rememberSaveable { mutableStateOf(spend.toString()) }
+    var isError by rememberSaveable { mutableStateOf(false) }
 
     Dialog(onDismissRequest = onClose) {
         Card {
@@ -182,7 +184,10 @@ fun EditCategoryDialog(
                     onValueChange = { titleInput = it },
                     label = { Text(text = stringResource(R.string.enter_title)) },
                     supportingText = { Text(stringResource(R.string.eg_grocery_home_rent_etc)) },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Next,
+                    ),
                     singleLine = true
                 )
                 TextField(
@@ -191,7 +196,10 @@ fun EditCategoryDialog(
                     onValueChange = { budgetInput = it },
                     label = { Text(stringResource(R.string.enter_budget)) },
                     supportingText = { Text(stringResource(R.string.eg_1000_2800_50)) },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next,
+                    ),
                 )
                 TextField(
                     modifier = Modifier.fillMaxWidth(),
@@ -199,10 +207,36 @@ fun EditCategoryDialog(
                     onValueChange = { spendAmountInput = it },
                     label = { Text(stringResource(R.string.enter_spend_amount)) },
                     supportingText = { Text(stringResource(R.string.eg_200_550_50)) },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done,
+                    ),
                 )
                 Spacer(modifier = Modifier.height(6.dp))
-                Button(onClick = { onClickUpdateCategory(titleInput, budgetInput.toFloat(), spendAmountInput.toFloat()) }) {
+
+                if (isError) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = stringResource(R.string.please_enter_a_valid_number),
+                        color = MaterialTheme.colorScheme.error,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(6.dp))
+                }
+
+                Button(onClick = {
+                    try {
+                        onClickUpdateCategory(
+                            titleInput,
+                            budgetInput.toFloat(),
+                            spendAmountInput.toFloat()
+                        )
+                        isError = false
+                    } catch (e: Exception) {
+                        isError = true
+                    }
+                }
+                ) {
                     Text(text = stringResource(R.string.update_category))
                 }
             }
