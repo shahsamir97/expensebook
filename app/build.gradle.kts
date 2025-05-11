ed9269c69aca3b2da42d5be491884670270122d1
Md. Shah Samir <mdshahsamir@gmail.com>
2025-05-11 07:43:11 +0600

    Update version to 1.0.1 (version code 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.samapplab.expensebook"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
